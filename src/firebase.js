f5a925653507fe4a4645b57eb333e785363f8744
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2024-03-26 23:44:50 +0530

    groceries-app

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZbi0ndqJqiW1YxU9X2VMXd6xM6YZVp_g",
+  authDomain: "ecommercereact-e3584.firebaseapp.com",
+  projectId: "ecommercereact-e3584",
+  storageBucket: "ecommercereact-e3584.appspot.com",
+  messagingSenderId: "539548083513",
+  appId: "1:539548083513:web:4070166e28f5aa191834e8",
+  measurementId: "G-MNHW5YDJ41"
+};
+
+export const app = initializeApp(firebaseConfig);
